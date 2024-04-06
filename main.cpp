b1e584b0fed353a14b26de196804c2d659a1cf88
markoduarte <162354537+markoduarte@users.noreply.github.com>
2024-04-05 20:26:23 -0400

    Altura e Idade em C++
    Código da aula de Linguagem de Programação

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int idade;
+    float altura;
+    cin >> idade;
+    cin >> altura;
+    cout << "Eu tenho " << idade << " anos" << endl;
+    cout << "E Minha altura eh " << altura << " metros";
+
+    return 0;
+}
