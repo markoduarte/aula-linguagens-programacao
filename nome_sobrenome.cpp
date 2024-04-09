8bf34c6bb92ca5c4815fdce38f44ecfe8f077a41
markoduarte <162354537+markoduarte@users.noreply.github.com>
2024-04-08 21:08:24 -0400

    Nome e Sobrenome
    Verifica a primeira e a última letra

---
+++ nome_sobrenome.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int teste = 34;
+    cout << teste << endl;
+    return 0;
+}
---
+++ nomesobrenome.py
@@ -0,0 +1,4 @@
+nome = input()
+sobrenome = input()
+ultima_letra = len(nome) - 1
+print(sobrenome, ",",nome[0], nome[ultima_letra], sep='')
