ced6fd36cd979515e061325527442fbb2a3073cb
markoduarte <162354537+markoduarte@users.noreply.github.com>
2024-04-10 21:33:52 -0400

    media while
    cálculo da média usando o while

---
+++ mediawhile.py
@@ -0,0 +1,16 @@
+notas=[]
+i = 1
+
+nota = float(input('nota[0]:'))
+
+while nota != -1:
+    notas.append(nota)
+    nota = float(input('nota[' + str(i) + ']'))
+    i+=1
+
+media = sum(notas) / len(notas)
+
+if media > 7:
+    print("aprovado")
+else:
+    print("reprovado")
